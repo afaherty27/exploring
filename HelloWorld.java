3f37c7ff6e7783ba9cee6b3e44a87e2b825cea28
Adam <afaherty27@gmail.com>
2015-05-11 18:38:01 -0500

    Add HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,16 @@
-
+/**
+ * Class to write Hello World to terminal.  File created in Git shell.
+ *    Written in Eclipse.  Uploaded to GitHub via Git shell
+ *    
+ * @author Adam Faherty
+ */
 public class HelloWorld {
 	
+	/**
+	 * Main processing class for program
+	 * 
+	 * @param args Reference to an array of String objects
+	 */
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
