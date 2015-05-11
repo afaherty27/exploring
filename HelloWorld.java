6da8684b947becf949149800b55f7620a32ca1b9
Adam <afaherty27@gmail.com>
2015-05-11 17:50:02 -0500

    Add HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+package exploring;
+
+/**
+ * HelloWorld class.  .java file created in Git shell command line. 
+ *   File written in Eclipse.  Uploaded to Github.
+ *   
+ *   @author Adam Faherty
+ */
+public class HelloWorld {
+	
+	/**
+	 * Main processing method for program
+	 * 
+	 * @param args Reference to String array
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+	}
+}
